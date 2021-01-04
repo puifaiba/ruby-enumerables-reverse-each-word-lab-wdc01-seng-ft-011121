86d56716600794ede11658bc7795fbda36667c07
Boonie Attawut <puifai.ba@gmail.com>
2021-01-04 22:43:07 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 def reverse_each_word(string)
-  word_array = []
-  word_array << string.split(" ")
+  word_array = string.split(" ")
+  reversed_array = word_array.each { |word| word.reverse!}
+  reversed_array.join(" ")
 end
 
 reverse_each_word("What are you doing?")
